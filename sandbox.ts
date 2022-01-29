cd1200d434ce6d39b77fdc2344f97b803470924a
raul <raul.borregomarques@gmail.com>
2022-01-28 21:49:35 -0800

    finish Lesson

---
+++ sandbox.ts
@@ -0,0 +1,6 @@
+const character = 'Raul'.toLocaleUpperCase();
+console.log(character);
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+inputs.forEach(input => console.log(input));
