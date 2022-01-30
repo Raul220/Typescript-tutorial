1e7ec1b8e9d20ad62a2444fda14f3823ace8dccb
raul <raul.borregomarques@gmail.com>
2022-01-29 16:21:09 -0800

    Finish Lesson 4

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,27 @@
 //arrays
+var names = ['Raul', 'Cynthia', 'Santino'];
+names.push('Sauron');
+//names.push(20);
+//names[0] = 3;
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+//numbers.push(true);
+var mixed = ['Raul', 32, 4, 5];
+mixed.push('Sauron');
+mixed.push(15);
+mixed[0] = 1;
+//mixed.push(true);
 //objects
+var person = {
+    name: 'Raul',
+    age: 32,
+    is_sick: false
+};
+person.name = 'Raul Adolfo';
+//person.age = 'String'; error => age has to be a number
+//person.skills = ['fuck', 'lick', 'winn']; error => skills wasn't declarated
+person = {
+    name: 'Cynthia',
+    age: 28,
+    is_sick: false
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,33 @@
 //arrays
+let names = ['Raul', 'Cynthia', 'Santino'];
+names.push('Sauron');
+//names.push(20);
+//names[0] = 3;
 
+let numbers = [10, 20, 30, 40];
+numbers.push(25);
+//numbers.push(true);
+
+let mixed = ['Raul', 32, 4, 5];
+mixed.push('Sauron');
+mixed.push(15);
+mixed[0] = 1;
+//mixed.push(true);
 
 //objects
+let person = {
+    name: 'Raul',
+    age: 32,
+    is_sick: false
+}
+
+person.name = 'Raul Adolfo';
+//person.age = 'String'; error => age has to be a number
+//person.skills = ['fuck', 'lick', 'winn']; error => skills wasn't declarated
+
+person = {
+    name: 'Cynthia',
+    age: 28,
+    is_sick: false,
+    //skills: []
+}
