ed1a7eb75b293b9d8fc087a837653c3341ccfd30
raul <raul.borregomarques@gmail.com>
2022-02-03 09:32:31 -0800

    Finish Lesson 16

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 const me = {
-    name: 'Luke Skywlaker',
+    name: "Luke Skywlaker",
     age: 23,
     speak(text) {
         console.log(text);
@@ -7,26 +7,40 @@ const me = {
     spend(amount) {
         console.log(`I spend ${amount}`);
         return amount;
-    }
+    },
 };
 let person;
 const greet_person = (person) => console.log(person.name);
 greet_person(me);
-import { Invoice } from './classes/Invoice.js';
-const invOne = new Invoice('Chubaka', 'Work on the Web Site', 20);
-const invTwo = new Invoice('Han Solo', 'Lickin princes pussy', 250);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-console.log(invoices);
-const form = document.querySelector('.new-item-form'); //Casting
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice("Luke", "Socking under the Bridge", 50);
+// docTwo = new Payment("Yoda", "Let Luke Socking under the Bridge", 1550);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
+// const invOne = new Invoice("Chubaka", "Work on the Web Site", 20);
+// const invTwo = new Invoice("Han Solo", "Lickin princes pussy", 250);
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// console.log(invoices);
+const form = document.querySelector(".new-item-form"); //Casting
 console.log(form.children);
 //inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    doc =
+        type.value === "invoice"
+            ? new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+            : new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,71 @@
 //interfaces
 interface IsPerson {
-    name: string;
-    age: number;
-    speak(a: string): void;
-    spend(a: number): number;
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
 }
 
 const me: IsPerson = {
-    name: 'Luke Skywlaker',
-    age: 23,
-    speak(text: string) {
-        console.log(text);
-    },
-    spend(amount: number) {
-        console.log(`I spend ${amount}`);
-        return amount;
-    }
-}
+  name: "Luke Skywlaker",
+  age: 23,
+  speak(text: string) {
+    console.log(text);
+  },
+  spend(amount: number) {
+    console.log(`I spend ${amount}`);
+    return amount;
+  },
+};
 
-let person : IsPerson;
+let person: IsPerson;
 
 const greet_person = (person: IsPerson) => console.log(person.name);
 
 greet_person(me);
 
-import {Invoice} from './classes/Invoice.js'
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-const invOne = new Invoice('Chubaka', 'Work on the Web Site', 20);
-const invTwo = new Invoice('Han Solo', 'Lickin princes pussy', 250);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
 
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
+// docOne = new Invoice("Luke", "Socking under the Bridge", 50);
+// docTwo = new Payment("Yoda", "Let Luke Socking under the Bridge", 1550);
 
-console.log(invoices);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
 
-const form = document.querySelector('.new-item-form') as HTMLFormElement; //Casting
+// console.log(docs);
+
+// const invOne = new Invoice("Chubaka", "Work on the Web Site", 20);
+// const invTwo = new Invoice("Han Solo", "Lickin princes pussy", 250);
+
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// console.log(invoices);
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement; //Casting
 console.log(form.children);
 
 //inputs
-const type = document.querySelector('#type') as HTMLSelectElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
-form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    );
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  let doc: HasFormatter;
+  doc =
+    type.value === "invoice"
+      ? new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+      : new Payment(tofrom.value, details.value, amount.valueAsNumber);
+
+  console.log(doc);
 });
